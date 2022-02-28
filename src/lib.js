f1bb8f93c823546030dd8ca59863c6eacc0122bc
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-02-27 23:33:17 -0500

    Code optimized

--- src/lib.js
+++ src/lib.js
@@ -1,4 +1,7 @@
-import Icon from "./components/Icon";
+import Carousel from "./components/Carousel";
 import HamburgerIcon from "./components/HamburgerIcon";
+import Icon from "./components/Icon";
+import MobNav from "./components/MobNav";
+import NavBar from "./components/NavBar";
 
-export { Icon, HamburgerIcon };
+export { Carousel, HamburgerIcon, Icon, MobNav, NavBar };
