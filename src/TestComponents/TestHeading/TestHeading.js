2719cb57a7b2ed3d30a44b8fcfdfd35c80ad872e
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-03-24 16:47:41 -0400

    Heading component works well

--- src/TestComponents/TestHeading/TestHeading.js
+++ src/TestComponents/TestHeading/TestHeading.js
@@ -3,25 +3,29 @@ import Heading from "Components/Heading";
 
 const TestHeading = () => {
   return (
-    <>  
-        <Heading className="textBlue" type={1} > 
-            Heading 1
-        </Heading>
-        <Heading className="textBlue" type={2} >
-            Heading 2
-        </Heading>
-        <Heading className="textBlue" type={3} > 
-            Heading 3
-        </Heading>
-        <Heading className="textBlue" type={4} >
-            Heading 4
-        </Heading>
-        <Heading className="textBlue" type={5} > 
-            Heading 5
-        </Heading>
-        <Heading className="textBlue" type={6} >
-            Heading 6
-        </Heading>
+    <>
+      <Heading
+        className="textBlue"
+        type={1}
+        onClick={() => console.log("Hello")}
+      >
+        Heading 1
+      </Heading>
+      <Heading className="textBlue" type={2}>
+        Heading 2
+      </Heading>
+      <Heading className="textBlue" type={3}>
+        Heading 3
+      </Heading>
+      <Heading className="textBlue" type={4}>
+        Heading 4
+      </Heading>
+      <Heading className="textBlue" type={5}>
+        Heading 5
+      </Heading>
+      <Heading className="textBlue" type={6}>
+        Heading 6
+      </Heading>
     </>
   );
 };
--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,21 +1,64 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Heading = ({
-  children,
-  type,
-  className,
-  ...props
-}) => {
+import defaultPropsMap from "Constants/defaultProps";
+const { defaultProps, defaultPropTypes } = defaultPropsMap;
 
-  switch(type) {
-    case 1:   return <h1 className={className} {...props} > {children} </h1>
-    case 2:   return <h2 className={className} {...props} > {children} </h2>
-    case 3:   return <h3 className={className} {...props} > {children} </h3>
-    case 4:   return <h4 className={className} {...props} > {children} </h4>
-    case 5:   return <h5 className={className} {...props} > {children} </h5>
-    case 6:   return <h6 className={className} {...props} > {children} </h6>
-    default:  return <h1 className={className} {...props} > {children} </h1>
+const Heading = ({ children, type, className, ...props }) => {
+  switch (type) {
+    case 1:
+      return (
+        <h1 className={className} {...props}>
+          {children}
+        </h1>
+      );
+    case 2:
+      return (
+        <h2 className={className} {...props}>
+          {children}
+        </h2>
+      );
+    case 3:
+      return (
+        <h3 className={className} {...props}>
+          {children}
+        </h3>
+      );
+    case 4:
+      return (
+        <h4 className={className} {...props}>
+          {children}
+        </h4>
+      );
+    case 5:
+      return (
+        <h5 className={className} {...props}>
+          {children}
+        </h5>
+      );
+    case 6:
+      return (
+        <h6 className={className} {...props}>
+          {children}
+        </h6>
+      );
+    default:
+      return (
+        <h1 className={className} {...props}>
+          {children}
+        </h1>
+      );
   }
+};
+
+Heading.propTypes = {
+  ...defaultPropTypes,
+  type: PropTypes.number,
+};
+
+Heading.defaultProps = {
+  ...defaultProps,
+  type: 1,
 };
 
 export default Heading;
