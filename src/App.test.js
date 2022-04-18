5f1e99fdc342d0bf6b86a36b27a3f2f79604c9d5
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-04-18 14:17:29 -0400

    Jest added and test runs properly

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("Renders app", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/desktop/i);
+  expect(linkElement).toBeInTheDocument();
+});
