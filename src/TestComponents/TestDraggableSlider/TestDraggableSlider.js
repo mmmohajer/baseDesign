d30dc255c1f40e3fb21acea70fa5cd98a994b11a
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-07-21 08:19:25 -0400

    SwipableSlider component added

--- src/TestComponents/TestDraggableSlider/TestDraggableSlider.js
+++ src/TestComponents/TestDraggableSlider/TestDraggableSlider.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import cx from 'classnames';
 
-import DraggableSlider from 'Components/DraggableSlider';
-import DraggableSliderItem from 'Components/DraggableSlider/subs/DraggableSliderItem';
+import SwipableSlider from 'Components/SwipableSlider';
+import SwipableSliderItem from 'Components/SwipableSlider/subs/SwipableSliderItem';
 
 import styles from './TestDraggableSlider.module.scss';
 
@@ -12,7 +12,7 @@ const TestDraggableSlider = () => {
   const [moveToItemWithNum, setMoveToItemWithNum] = useState(1);
   return (
     <>
-      <DraggableSlider
+      <SwipableSlider
         moveRight={moveRight}
         setMoveRight={setMoveRight}
         moveLeft={moveLeft}
@@ -22,60 +22,60 @@ const TestDraggableSlider = () => {
         transitionDuration={0.25}
         transition_timing_function="ease-in"
         className={cx(styles.container)}>
-        <DraggableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
+        <SwipableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
           <p className="bgSilver textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 1
           </p>
           <p className="bgPrimary textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 2
           </p>
-        </DraggableSliderItem>
+        </SwipableSliderItem>
 
-        <DraggableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
+        <SwipableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
           <p className="bgSilver textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 3
           </p>
           <p className="bgPrimary textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 4
           </p>
-        </DraggableSliderItem>
+        </SwipableSliderItem>
 
-        <DraggableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
+        <SwipableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
           <p className="bgSilver textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 5
           </p>
           <p className="bgPrimary textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 6
           </p>
-        </DraggableSliderItem>
+        </SwipableSliderItem>
 
-        <DraggableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
+        <SwipableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
           <p className="bgSilver textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 7
           </p>
           <p className="bgPrimary textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 8
           </p>
-        </DraggableSliderItem>
+        </SwipableSliderItem>
 
-        <DraggableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
+        <SwipableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
           <p className="bgSilver textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 9
           </p>
           <p className="bgPrimary textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 10
           </p>
-        </DraggableSliderItem>
+        </SwipableSliderItem>
 
-        <DraggableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
+        <SwipableSliderItem className="w-per-100 flex flex--jc--between flex--ai--center">
           <p className="bgSilver textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 11
           </p>
           <p className="bgPrimary textRed w-per-50 flex flex--jc--center flex--ai--center height-px-100">
             Item 12
           </p>
-        </DraggableSliderItem>
-      </DraggableSlider>
+        </SwipableSliderItem>
+      </SwipableSlider>
       <button onClick={() => setMoveRight(true)}>Right</button>
       <button onClick={() => setMoveLeft(true)}>Left</button>
       <button onClick={() => setMoveToItemWithNum(4)}>Jump To number 4</button>
