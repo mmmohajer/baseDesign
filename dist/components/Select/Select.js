4242ac9436d4c657f9fe16864a774b1641d7b753
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-07-22 20:15:40 -0400

    Search component updated

--- dist/components/Select/Select.js
+++ dist/components/Select/Select.js
@@ -2,6 +2,8 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
+var _typeof = require("@babel/runtime/helpers/typeof");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -11,9 +13,11 @@ var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/de
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
+var _slicedToArray2 = _interopRequireDefault(require("@babel/runtime/helpers/slicedToArray"));
+
 var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutProperties"));
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 var _classnames = _interopRequireDefault(require("classnames"));
 
@@ -21,7 +25,17 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 
 var _defaultProps = _interopRequireDefault(require("../../constants/defaultProps"));
 
-var _excluded = ["options", "className", "optionClassName", "fullWidth", "children"];
+var _Icon = _interopRequireDefault(require("../Icon"));
+
+var _Search = _interopRequireDefault(require("../Search"));
+
+var _SelectModule = _interopRequireDefault(require("./Select.module.scss"));
+
+var _excluded = ["options", "className", "optionClassName", "fullWidth", "children", "selectValue", "setSelectValue", "arrowIconFillColor", "arrowIconStrokeColor", "arrowIconScale", "searchIconFillColor", "searchIconStrokeColor", "optinsContainerClassName"];
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 
@@ -36,18 +50,73 @@ var Select = /*#__PURE__*/_react["default"].forwardRef(function (_ref, ref) {
       optionClassName = _ref.optionClassName,
       fullWidth = _ref.fullWidth,
       children = _ref.children,
+      selectValue = _ref.selectValue,
+      setSelectValue = _ref.setSelectValue,
+      arrowIconFillColor = _ref.arrowIconFillColor,
+      arrowIconStrokeColor = _ref.arrowIconStrokeColor,
+      arrowIconScale = _ref.arrowIconScale,
+      searchIconFillColor = _ref.searchIconFillColor,
+      searchIconStrokeColor = _ref.searchIconStrokeColor,
+      optinsContainerClassName = _ref.optinsContainerClassName,
       props = (0, _objectWithoutProperties2["default"])(_ref, _excluded);
-  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement("select", (0, _extends2["default"])({
-    className: (0, _classnames["default"])(fullWidth && 'w-per-100', className)
+
+  var _useState = (0, _react.useState)(options),
+      _useState2 = (0, _slicedToArray2["default"])(_useState, 2),
+      filteredOptions = _useState2[0],
+      setFilteredOptions = _useState2[1];
+
+  var _useState3 = (0, _react.useState)('Select'),
+      _useState4 = (0, _slicedToArray2["default"])(_useState3, 2),
+      curVal = _useState4[0],
+      setCurVal = _useState4[1];
+
+  var _useState5 = (0, _react.useState)(false),
+      _useState6 = (0, _slicedToArray2["default"])(_useState5, 2),
+      isOptionsActive = _useState6[0],
+      setIsOptionsActive = _useState6[1];
+
+  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement("div", (0, _extends2["default"])({
+    className: (0, _classnames["default"])('pos-rel', fullWidth && 'w-per-100', className, children)
   }, props, {
     ref: ref
-  }), options === null || options === void 0 ? void 0 : options.map(function (item, idx) {
-    return /*#__PURE__*/_react["default"].createElement("option", {
-      className: optionClassName,
+  }), !isOptionsActive ? /*#__PURE__*/_react["default"].createElement("div", {
+    className: (0, _classnames["default"])(_SelectModule["default"].select),
+    onClick: function onClick() {
+      return setIsOptionsActive(!isOptionsActive);
+    }
+  }, curVal) : /*#__PURE__*/_react["default"].createElement("input", {
+    type: "search",
+    closable: false,
+    onChange: function onChange(e) {
+      return setFilteredOptions(function () {
+        return options.filter(function (x) {
+          var _x$shownText, _x$shownText$toLowerC, _e$target$value;
+
+          return x === null || x === void 0 ? void 0 : (_x$shownText = x.shownText) === null || _x$shownText === void 0 ? void 0 : (_x$shownText$toLowerC = _x$shownText.toLowerCase()) === null || _x$shownText$toLowerC === void 0 ? void 0 : _x$shownText$toLowerC.includes((_e$target$value = e.target.value) === null || _e$target$value === void 0 ? void 0 : _e$target$value.toLowerCase());
+        });
+      });
+    }
+  }), /*#__PURE__*/_react["default"].createElement("div", {
+    className: (0, _classnames["default"])(_SelectModule["default"].optionContainer, isOptionsActive && _SelectModule["default"].optionContainerIsActive, optinsContainerClassName)
+  }, filteredOptions === null || filteredOptions === void 0 ? void 0 : filteredOptions.map(function (item, idx) {
+    return /*#__PURE__*/_react["default"].createElement("div", {
+      className: (0, _classnames["default"])(_SelectModule["default"].option, optionClassName),
       key: idx,
-      value: item === null || item === void 0 ? void 0 : item.value
+      value: item === null || item === void 0 ? void 0 : item.value,
+      onClick: function onClick() {
+        setSelectValue(item.value);
+        setCurVal(item.shownText);
+        setIsOptionsActive(false);
+      }
     }, item === null || item === void 0 ? void 0 : item.shownText);
-  })));
+  })), /*#__PURE__*/_react["default"].createElement("div", {
+    className: (0, _classnames["default"])(_SelectModule["default"].arrowConrainer)
+  }, /*#__PURE__*/_react["default"].createElement(_Icon["default"], {
+    type: "down",
+    fill: arrowIconFillColor,
+    stroke: arrowIconStrokeColor,
+    scale: arrowIconScale
+  }))));
 });
 
 Select.propTypes = _objectSpread(_objectSpread({}, defaultPropTypes), {}, {
