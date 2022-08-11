eda6cc3800db049707483516926ddc39fea0a652
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-08-11 01:27:48 -0400

    Extra files deleted

--- dist/constants/vars.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.CSS_CLASS_RANDOM_LENGTH = void 0;
-var CSS_CLASS_RANDOM_LENGTH = 10;
-exports.CSS_CLASS_RANDOM_LENGTH = CSS_CLASS_RANDOM_LENGTH;
-//# sourceMappingURL=vars.js.map
