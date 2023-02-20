b97bb7157f6b36cbc709e611f4888b8eb82bcc8b
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-02-19 23:13:04 -0500

    Subnav active class added

--- src/components/MobNav/subs/MobSubNavItem.js
+++ src/components/MobNav/subs/MobSubNavItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 
-const MobSubNavItem = ({ children, className, ...props }) => {
+const MobSubNavItem = ({ children, isActive, activeClassName, className, ...props }) => {
   return (
     <>
       <div {...props} className={cx('iswad_mobSubNavItem', className, isActive && activeClassName)}>
