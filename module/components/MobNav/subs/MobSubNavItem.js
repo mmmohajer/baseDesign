fc2c5b3a2726f92f45c4d7463f3fed7a4699d3ae
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-02-19 23:13:40 -0500

    Subnav active class added

--- module/components/MobNav/subs/MobSubNavItem.js
+++ module/components/MobNav/subs/MobSubNavItem.js
@@ -15,10 +15,12 @@ var _react = _interopRequireDefault(require("react"));
 
 var _classnames = _interopRequireDefault(require("classnames"));
 
-var _excluded = ["children", "className"];
+var _excluded = ["children", "isActive", "activeClassName", "className"];
 
 var MobSubNavItem = function MobSubNavItem(_ref) {
   var children = _ref.children,
+      isActive = _ref.isActive,
+      activeClassName = _ref.activeClassName,
       className = _ref.className,
       props = (0, _objectWithoutProperties2["default"])(_ref, _excluded);
   return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement("div", (0, _extends2["default"])({}, props, {
