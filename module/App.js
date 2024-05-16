c2e4382f8a9645df6ed05247b98dee6ac0393bcb
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2024-05-16 19:46:34 -0400

    More css classes exist

--- module/App.js
+++ module/App.js
@@ -68,9 +68,7 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 var Home = function Home() {
-  return /*#__PURE__*/_react["default"].createElement("div", {
-    className: ""
-  });
+  return /*#__PURE__*/_react["default"].createElement("div", null);
 };
 
 var _default = Home;
